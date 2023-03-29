50798ddda93b9ddbdfdbcb47b63547d2256d69cd
mariadddd <128831787+mariadddd@users.noreply.github.com>
2023-03-29 19:45:33 +0300

    Add files via upload

---
+++ MinMax.cs
@@ -0,0 +1,10 @@
+﻿namespace MinMax
+{
+    internal class MinMax<T>
+    {
+        T[] _data;
+        public MinMax(T[] data) {_data = data;}
+        public T Min() => _data.Min();
+        public T Max() => _data.Max();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MinMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] ints = { 1, 2, 8, 4 };
+            string[] strings = { "hhh", "jjjjjjjj", "k" };
+            MinMax<int> minMax = new(ints);
+            MinMax<string> minMax2 = new(strings);
+            Console.WriteLine(minMax.Min());
+            Console.WriteLine(minMax.Max());
+            Console.WriteLine(minMax2.Min());
+            Console.WriteLine(minMax2.Max());
+        }
+    }
+}
